04d16fb6236ba76d530cac8462cbcc376e17b7e6
rachit-j <rjaiswal.sd.77@gmail.com>
2023-10-22 21:44:26 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
+        <title>Stocktify Backend</title>
         <link rel="icon" href="/dog.png" />
       </Head>
 
