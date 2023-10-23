7d4888681ad57ab3135f6d94ef688e69bfe2e0ff
rachit-j <rjaiswal.sd.77@gmail.com>
2023-10-22 18:55:53 -0700

    Added token limits

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -47,6 +47,7 @@ export default async function (req, res) {
       model: "text-davinci-003",
       prompt: generatePrompt(stockbot),
       temperature: 0.6,
+      max_tokens: 500,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
